75b18eede79fa2a679a4b853513ed3b652b42d9a
ujjwal-singh26 <singhujjwal1703@gmail.com>
2024-10-15 14:09:41 +0530

    Updated EJS files and index.js for enhanced functionality

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080; // Use environment variable for port
 const path = require("path");
 const { v4: uuidv4 } = require('uuid');
 const methodOverride = require("method-override");
 
 app.use(methodOverride("_method"));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 // ----------------------------------------------------------------------------------------------------------------------------------
 
@@ -230,54 +230,53 @@ let posts = [
 
 // ----------------------------------------------------------------------------------------------------------------------------------
 
-app.get("/posts",(req,res)=>{
-    res.render("index.ejs",{posts});
+
+// Update all redirects and renders to use relative URLs
+app.get("/posts", (req, res) => {
+    res.render("index.ejs", { posts });
 });
 
-app.get("/posts/new",(req,res)=>{
-    res.render("new.ejs",{posts});
+app.get("/posts/new", (req, res) => {
+    res.render("new.ejs", { posts });
 });
 
 app.post("/posts", (req, res) => {
-    let { titlename, dir_name, description, poster ,release, genere, ratings} = req.body;
+    let { titlename, dir_name, description, poster, release, genere, ratings } = req.body;
     let id = uuidv4();
-    posts.push({ id, title: titlename, director: dir_name, description, poster ,release, genere, ratings});
-    res.redirect("/posts");
+    posts.push({ id, title: titlename, director: dir_name, description, poster, release, genere, ratings });
+    res.redirect("/posts"); // This is already correct
 });
 
-app.get("/posts/:id",(req,res)=>{
-    let {id}=req.params;
-    let post=posts.find((p)=> id === p.id);
-    if(!post)
-    {
+app.get("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => id === p.id);
+    if (!post) {
         res.render("error.ejs");
     }
-    console.log(post);
-    res.render("show.ejs",{post});
+    res.render("show.ejs", { post });
 });
 
-app.patch("/posts/:id",(req,res)=>{
-    let {id}=req.params;
-    let newContent=req.body.content;
-    let post=posts.find((p)=> id === p.id);
-    post.content=newContent;
-    console.log(post);
+app.patch("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p) => id === p.id);
+    post.content = newContent;
     res.redirect("/posts");
 });
 
-app.get("/posts/:id/edit",(req,res)=>{
-    let {id}=req.params;
-    let post=posts.find((p)=> id === p.id);
-    res.render("edit.ejs",{post});
+app.get("/posts/:id/edit", (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("edit.ejs", { post });
 });
 
-app.delete("/posts/:id",(req,res)=>{
-    let {id}=req.params;
-    posts=posts.filter((p)=> id != p.id);
+app.delete("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    posts = posts.filter((p) => id != p.id);
     res.redirect("/posts");
 });
 
-app.listen(port,()=>{
+// Use the port from environment variable
+app.listen(port, () => {
     console.log(`Listening to port: ${port}`);
 });
-
